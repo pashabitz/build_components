f3704f893716ef6b11d343b3ddc3c8a1a7fc8f2c
Pasha Bitz <pasha@convex.dev>
2024-08-06 16:15:44 -0700

    Fix ts error

--- convex/app.config.ts
+++ convex/app.config.ts
@@ -3,6 +3,6 @@ import webCrawler from "../web_crawler/component.config";
 
 const app = defineApp();
 const crawlerComponent = app.install(webCrawler, { name: "webCrawler", args: {} });
-app.mount({ webCrawler: crawlerComponent.exports });
+app.mount({ webCrawler: crawlerComponent.exports as any });
 app.mountHttp("/crawler/", crawlerComponent);
 export default app;
